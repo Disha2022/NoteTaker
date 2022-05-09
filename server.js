bd8f42e94ecc4d105ee843feb82d3a99f33a383b
Disha2022 <99584117+Disha2022@users.noreply.github.com>
2022-05-08 21:40:22 -0400

    added PORT

--- server.js
+++ server.js
@@ -62,6 +62,6 @@ app.get('/',(req,res)=>{
 
 //Listen=============================================================
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
